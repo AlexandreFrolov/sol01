326fd0513de2bfbd7173abbddcaaa7e82380582d
Alexandre Frolov <alexandre@frolov.pp.ru>
2021-11-29 03:43:27 -0800

    Add nodejs and web3

--- les03/start_node.sh
+++ les03/start_node.sh
@@ -1,1 +1,1 @@
-geth --datadir ~/node1 --nodiscover --mine --miner.threads 1 --maxpeers 0 --verbosity 3 --networkid 98760 console
+geth --datadir ~/node1 --nodiscover --mine --miner.threads 1 --maxpeers 0 --verbosity 3 --networkid 98760 --http --http.api personal,eth,net,web3 console
