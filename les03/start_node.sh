e1ba56e127efbff4842cbc1f84a6b820c0a4fd97
Alexandre Frolov <alexandre@frolov.pp.ru>
2021-11-30 07:21:05 -0800

    Fix IPC

--- les03/start_node.sh
+++ les03/start_node.sh
@@ -1,1 +1,1 @@
-geth --datadir ~/node1 --nodiscover --mine --miner.threads 1 --maxpeers 0 --verbosity 3 --networkid 98760 --http --http.api personal,eth,net,web3 console
+geth --datadir ~/node1 --ipcpath "~/node1/geth.ipc" --nodiscover --mine --miner.threads 1 --maxpeers 0 --verbosity 3 --networkid 98760 console
