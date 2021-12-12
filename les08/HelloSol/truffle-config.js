5abb174c21749717a1e1138b2869dc009f581450
Alexandre Frolov <alexandre@frolov.pp.ru>
2021-12-12 02:48:14 -0800

    Add Rinkeby

--- les08/HelloSol/truffle-config.js
+++ les08/HelloSol/truffle-config.js
@@ -10,8 +10,15 @@ module.exports = {
       port: 8545,
       network_id: 98760,
       gas: 5000000
+    },
+  rinkeby: {
+      host: "localhost",
+      port: 8545,
+      from: "0xaf3cc1db3ef0dce4c1355e87ce11bb1c68fb0ed4",
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
-  },
+},
   mocha: {
   },
   compilers: {
