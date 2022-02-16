2aa7df4245edadf7caa869f732be0907f201318a
Alexandre Frolov <alexandre@frolov.pp.ru>
2022-02-16 15:28:17 +0000

    Fix les08

--- les08/hello_sol_get_tr_light.js
+++ les08/hello_sol_get_tr_light.js
@@ -4,21 +4,17 @@
 let Web3 = require('web3')
 let fs = require('fs')
 let request = require('request')
-
 var contract_name = process.argv[2];
 var network_id = process.argv[3];
-
 console.log('Contract script: ' + contract_name);
-
 var path = require('path');
 var contractJSON = require(path.join(__dirname, 'HelloSol/build/contracts/' + contract_name + '.json'));
 var decoded = JSON.parse(JSON.stringify(contractJSON.networks, undefined, 2));
 var contract_address = decoded[network_id].address;
 var abi = contractJSON.abi;
-
 console.log('contract_address: ' +  contract_address);
-
 var web3;
+
 if(network_id == 5777)
 {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
@@ -32,25 +28,25 @@ var version = web3.version;
 console.log('Web3 version: ' + version);
 
 let myContract = new web3.eth.Contract(abi, contract_address);
-
-  myContract.methods.getValue().call({from: contract_address}, (error, result) =>
-  {
-     if(!error){
-       console.log('getValue result: ' + result);
-       console.log(JSON.stringify(result, undefined, 2));
-     } else{
-       console.log(error);
-     }
-  });
-
-  myContract.methods.getString().call({from: contract_address}, (error, result) =>
-  {
-     if(!error){
-       console.log('getString result: ' + result);
-     } else{
-       console.log(error);
-     }
-  })
+myContract.methods.getValue().call({from: contract_address}, (error, result) =>
+{
+   if(!error){
+     console.log('getValue result: ' + result);
+     console.log(JSON.stringify(result, undefined, 2));
+   } else{
+     console.log(error);
+   }
+})
+.then(function () {
+    myContract.methods.getString().call({from: contract_address}, (error, result) =>
+    {
+       if(!error){
+         console.log('getString result: ' + result);
+       } else{
+         console.log(error);
+       }
+    })
+})
 .catch(function (error) {
   console.error(error);
 });
--- les08/hello_sol_set_tr_light.js
+++ les08/hello_sol_set_tr_light.js
@@ -1,27 +1,19 @@
 // node hello_sol_set_tr.js HelloSol 5777 ""
 // node hello_sol_set_tr.js HelloSol 98760 ""
 // node hello_sol_set_tr.js HelloSol 4 ""
-let Web3 = require('web3')
-let fs = require('fs')
-let request = require('request')
-
-var contract_name = process.argv[2];
-var network_id = process.argv[3];
-var unlock_password = process.argv[4];
-
-console.log('Contract script: ' + contract_name);
-
-var path = require('path');
-var contractJSON = require(path.join(__dirname, 'HelloSol/build/contracts/' + contract_name + '.json'));
-
-var decoded = JSON.parse(JSON.stringify(contractJSON.networks, undefined, 2));
-var contract_address = decoded[network_id].address;
-
-
-var abi = contractJSON.abi;
+const Web3 = require('web3')
+const fs = require('fs')
+const request = require('request')
+const contract_name = process.argv[2];
+const network_id = process.argv[3];
+const unlock_password = process.argv[4];
+const path = require('path');
+const contractJSON = require(path.join(__dirname, 'HelloSol/build/contracts/' + contract_name + '.json'));
+const decoded = JSON.parse(JSON.stringify(contractJSON.networks, undefined, 2));
+const contract_address = decoded[network_id].address;
+const abi = contractJSON.abi;
 console.log('contract_address: ' +  contract_address);
-
-var web3;
+let web3;
 if(network_id == 5777)
 {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
@@ -31,38 +23,22 @@ else
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
-var version = web3.version;
-console.log('Web3 version: ' + version);
-
 let myContract = new web3.eth.Contract(abi, contract_address);
-var myCoinbase;
+let myAddress;
 
 web3.eth.getAccounts()
 .then(accList => {
   return accList;
 })
-.then(function (accounts) {
-  var balancePromeses = [];
-  for(let i = 0; i < accounts.length; i++) {
-    balancePromeses[i] = web3.eth.getBalance(accounts[i]);
-  }
-
-  Promise.all(balancePromeses).then(values => {
-    for(let i = 0; i < values.length; i++) {
-      console.log('Account: ', accounts[i], 'balance: ', values[i], 'wei, ', web3.utils.fromWei(values[i], 'ether'), 'ether');
-    }
-  });
-  return accounts[0];
-})
 .then(function (account) {
-  console.log('account: ' +  account);
-  myCoinbase = account;
-  return web3.eth.personal.unlockAccount(account, unlock_password, 600)
+  console.log('account: ' +  account[0]);
+  myAddress = account[0];
+  return web3.eth.personal.unlockAccount(myAddress, unlock_password, 600)
 })
 .then(function (unlocked) {
   console.log('Unlocked: ' + unlocked);
 
-  myContract.methods.setValue(307777309).send({from: myCoinbase})
+  myContract.methods.setValue(307777309).send({from: myAddress})
   .once('setValue transactionHash', (hash) => {
     console.log('hash: ' + hash);
   })
@@ -73,14 +49,14 @@ web3.eth.getAccounts()
     console.log(JSON.stringify(receipt, undefined, 2));
   })
   .then(function () {
-  myContract.methods.setString("Тестовая строка 3777739").send({from: myCoinbase})
-  .once('transactionHash', (hash) => {
-    console.log('setString hash: ' + hash);
+    myContract.methods.setString("Тестовая строка 3777739").send({from: myAddress})
+    .once('transactionHash', (hash) => {
+      console.log('setString hash: ' + hash);
+    })
+    .on('receipt', (receipt) => {
+      console.log(JSON.stringify(receipt, undefined, 2));
+    })
   })
-  .on('receipt', (receipt) => {
-    console.log(JSON.stringify(receipt, undefined, 2));
-  })
-})
 })
 .catch(function (error) {
   console.error(error);
